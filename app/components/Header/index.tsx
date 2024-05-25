a125d577ab70fed0c8456df328eca67bd4031cf6
frederico lopes <fredericolopes@fredericos-MacBook-Pro.local>
2024-05-24 21:39:17 -0300

    feat: logout

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,31 +1,46 @@
 import { Text, View, Image, TouchableOpacity } from "react-native";
 
-import { styles } from "./styles";
+import { useUserStore } from "@/store";
 
 import hamburguer from "@/assets/icons/hamburguer.png";
+import logout from "@/assets/icons/logout.png";
 import user from "@/assets/icons/user.png";
+
+import { styles } from "./styles";
+import { useToasts } from "@/utils/services/toast";
 
 type HeaderProps = {
   name: string;
 };
 
 export const Header = ({ name }: HeaderProps) => {
+  const { setUser } = useUserStore();
+
+  function handleLogout() {
+    setUser(null);
+    useToasts({
+      type: "success",
+      title: "Logout",
+      message: "Você foi desconectado",
+    });
+  }
+
   return (
     <View style={styles.container}>
       <TouchableOpacity>
         <Image
-          source={hamburguer}
+          source={user}
           resizeMode="contain"
           style={{
-            width: 30,
-            height: 30,
+            width: 40,
+            height: 40,
           }}
         />
       </TouchableOpacity>
       <Text style={styles.title}>{name}</Text>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleLogout}>
         <Image
-          source={user}
+          source={logout}
           resizeMode="contain"
           style={{
             width: 40,
